8366711c9b28a1a0b4924b7b0d563e1957e190ce
Aether <2085163736@qq.com>
2025-03-31 20:18:55 +0800

    add InetAddress class

---
+++ net/InetAddress.h
@@ -0,0 +1,46 @@
+#include<string>
+#include<arpa/inet.h>
+#include<memory.h>
+
+namespace ilib{
+namespace net{
+class InetAddress
+{
+public:
+    explicit InetAddress(unsigned int port = 0);
+    InetAddress(const std::string &ip,unsigned int port);
+    InetAddress(const sockaddr_in & addr);
+
+    std::string toIp() const;
+    unsigned int toPort() const;
+    std::string toIpPort() const;
+
+    const sockaddr * getSockAddr() const {
+        return reinterpret_cast<const sockaddr *>(&addr_);
+    }
+
+    socklen_t getSockLen() const { return sizeof(addr_); };
+private:
+    sockaddr_in addr_;
+};
+
+InetAddress::InetAddress(unsigned int port = 0) {
+    memset(&addr_,0,getSockLen());
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = htons(port);
+    addr_.sin_addr.s_addr = INADDR_ANY;
+}
+
+InetAddress::InetAddress(const std::string &ip,unsigned int port) {
+    memset(&addr_,0,getSockLen());
+    addr_.sin_family = AF_INET;
+    inet_pton(AF_INET,ip.c_str(),&addr_.sin_addr);
+    addr_.sin_port = htons(port);
+}
+
+InetAddress::InetAddress(const sockaddr_in & addr) {
+    memcpy(&addr_,&addr,sizeof(addr_));
+}
+
+}
+}
